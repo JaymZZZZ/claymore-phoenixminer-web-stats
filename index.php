9fbc6beafa0291a0fa7378b5a712376b62d5d33f
JaymZZZZ <77397996+JaymZZZZ@users.noreply.github.com>
2021-04-12 09:41:52 -0500

    Update index.php
    Remove Codepen references

--- index.php
+++ index.php
@@ -44,14 +44,9 @@ $parser->parse_all_json_rpc_calls();
 <html lang='en' class=''>
 <head>
 	<title><?php echo $parser->miner_count ?> Miners: <?php echo $parser->global_hashrate ?> MH/s</title>
-	<script src='//production-assets.codepen.io/assets/editor/live/console_runner-079c09a0e3b9ff743e39ee2d5637b9216b3545af0de366d4b9aad9dc87e26bfd.js'></script>
-	<script src='//production-assets.codepen.io/assets/editor/live/events_runner-73716630c22bbc8cff4bd0f07b135f00a0bdc5d14629260c3ec49e5606f98fdd.js'></script>
-	<script src='//production-assets.codepen.io/assets/editor/live/css_live_reload_init-2c0dc5167d60a5af3ee189d570b1835129687ea2a61bee3513dee3a50c115a77.js'></script>
 	<meta charset='UTF-8'>
 	<meta name="robots" content="noindex">
 	<meta http-equiv="refresh" content="<?php echo $refresh_interval ?>">
-	<link rel="shortcut icon" type="image/x-icon" href="//production-assets.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico"/>
-	<link rel="mask-icon" type="" href="//production-assets.codepen.io/assets/favicon/logo-pin-f2d2b6d2c61838f7e76325261b7195c27224080bc099486ddd6dccb469b8e8e6.svg" color="#111"/>
 	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
 	<style class="cp-pen-styles">@import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700);
 
